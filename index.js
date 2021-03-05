8d9cebc3eda201ed521ac752fa1dcc8f6f8efac6
Achint Singh <achintsingh97@gmail.com>
2021-03-05 11:28:40 -0500

    Added js file and updated the css of the navbar

---
+++ index.js
@@ -0,0 +1,9 @@
+const navigationLinks = document.getElementsByClassName("navlinks");
+
+for (item of navigationLinks) {
+  item.addEventListener("mouseover", () => {
+    console.log(
+      "This is placeholder for a future mouse over action to be implemented"
+    );
+  });
+}
